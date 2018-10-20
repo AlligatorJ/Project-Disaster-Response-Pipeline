0effe7142a0a60c74e8a14eb5b835d82c9acbd86
AlligatorJ <35658305+AlligatorJ@users.noreply.github.com>
2018-10-20 14:16:15 +0200

    Updated ETL pipeline

--- disaster_response_pipeline_project/data/process_data.py
+++ disaster_response_pipeline_project/data/process_data.py
@@ -1,23 +1,68 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
-
+	'''	read and merge two dataframes
+		
+		input: 
+		messages_filepath: a csv file
+		categories_filepath: a csv file
+		
+		output:
+		pandas dataframe
+	
+	'''
+	messages = pd.read_csv(messages_filepath)
+	categories = pd.read_csv(categories_filepath)
+	df = messages.merge(categories)
+	return df
+	
 def clean_data(df):
-    pass
-
+	'''
+	cleans data for machine learning model
+	input:
+		df: a pandas dataframe
+	output:
+		a pandas dataframe
+	'''
+    # create a dataframe of the 36 individual category columns
+	categories = df.categories.str.split(';', expand = True)
+	# select the first row of the categories dataframe
+	row = categories[:1]
+	# use this row to extract a list of new column names for categories.
+	category_colnames = row.apply(lambda x: x.str.split('-')[0][0])
+	# rename the columns of `categories`
+	categories.columns = category_colnames
+	
+	#Convert category values to just numbers 0 or 1
+	for column in categories:
+		# set each value to be the last character of the string
+		categories[column] = categories[column].apply(lambda x: x.split('-')[1] if int(x.split('-')[1]) < 2 else 1)
+		# convert column from string to numeric
+		categories[column] = categories[column].astype(int)
+	
+	# drop the original categories column from `df`
+	df.drop('categories', axis = 1, inplace = True)
+	# concatenate the original dataframe with the new `categories` dataframe
+	df = pd.concat((df , categories),axis = 1)
+	#Remove duplicates
+	df = df.drop_duplicates()
+	
+	return df
 
 def save_data(df, database_filename):
-    pass  
+    table_name = 'messages'
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    #Save the clean dataset into an sqlite database
+    df.to_sql(table_name, engine, index=False, if_exists='replace')
 
 
 def main():
     if len(sys.argv) == 4:
-
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
-
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
